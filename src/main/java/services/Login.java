910c6a2f5b7f33916985b19ea15159157aa776c4
cbudtz <chrbudtz@gmail.com>
2018-02-14 11:11:07 +0100

    Fixed missing import

--- src/main/java/services/Login.java
+++ src/main/java/services/Login.java
@@ -3,17 +3,15 @@ package services;
 import com.github.mustachejava.DefaultMustacheFactory;
 import com.github.mustachejava.Mustache;
 import com.github.mustachejava.MustacheFactory;
-import util.FileLoader;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.util.HashMap;
-import java.util.Map;
 
 @Path("login")
 public class Login {
